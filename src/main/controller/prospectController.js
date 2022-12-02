e50a13aac26bdc3b20cac94114d0f47d51a47a53
NayyarAbhi <abhinayyar83@gmail.com>
2022-12-02 14:11:47 +0000

    prospect update

--- src/main/controller/prospectController.js
+++ src/main/controller/prospectController.js
@@ -3,6 +3,8 @@ const TABLES = require('../variables/tables.js').TABLES;
 const HTTP = require('../variables/status.js').HTTP;
 let PROSPECT_QUERY = require('../variables/prospect_sql.js').QUERY;
 
+const PRIMARY_KEY = 'CustomerId';
+
 // checking if a record present in the DB
 async function isRecordPresent(customerId) {
     const query = PROSPECT_QUERY.COUNT
@@ -11,6 +13,19 @@ async function isRecordPresent(customerId) {
 
     return (await db.getRecord(query))
         .recordset[0].RECORD_COUNT !== 0 ? true : false;
+}
+
+// getting fields from payload, which needs to be updated
+function getUpdateFields(obj) {
+    let update_fields = ''
+    const lastItem = Object.values(obj).pop();
+    for (let [key, value] of Object.entries(obj)) {
+        if (key !== PRIMARY_KEY) {
+            update_fields += (key + "='" + value + "'")
+            update_fields += (value !== lastItem) ? ',' : '';
+        }
+    }
+    return update_fields
 }
 
 // returning the prospect, if the customer id exist in the system
@@ -48,8 +63,8 @@ async function createProspect(req,res) {
             .replace('<domusCookieId>', req.body.DomusCookieId)
             .replace('<iBLogon>', req.body.IBLogon)
             .replace('<customerId>', req.body.customerId);
-        
-            console.log(insertQuery);
+
+        console.log(insertQuery);
         const result = await db.insertRecord(insertQuery);
         res.status(HTTP.OK.code)
             .json({ message: `Customer id ${customerId} is created successfully` })
@@ -64,11 +79,7 @@ async function updateProspect(req, res) {
     if (await isRecordPresent(customerId)) {
         const updateQuery = PROSPECT_QUERY.UPDATE
             .replace('<tableName>', TABLES.PROSPECT)
-            .replace('<cookie>', req.body.Cookie)
-            .replace('<sessionId>', req.body.SessionId)
-            .replace('<otpEmailId>', req.body.OtpEmailId)
-            .replace('<domusCookieId>', req.body.DomusCookieId)
-            .replace('<iBLogon>', req.body.IBLogon)
+            .replace('<update_fields>', getUpdateFields(req.body))
             .replace('<customerId>', customerId);
 
         const result = await db.updateRecord(updateQuery);
