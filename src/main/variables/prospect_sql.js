3ff20e75ee06bb55f34047f64fdb67a84e2885a5
Laksh Babbar <43206740+lakshbabbar@users.noreply.github.com>
2022-12-02 19:17:09 +0530

    Update prospect_sql.js

--- src/main/variables/prospect_sql.js
+++ src/main/variables/prospect_sql.js
@@ -2,7 +2,7 @@ const QUERY = {
     COUNT: "SELECT COUNT(*) as RECORD_COUNT FROM <tableName> WHERE CustomerId='<customerId>'",
     SELECT: "SELECT * FROM <tableName> WHERE CustomerId='<customerId>'",
     UPDATE: "UPDATE <tableName> SET Cookie='<cookie>', SessionId='<sessionId>', OtpEmailId='<otpEmailId>', DomusCookieId='<domusCookieId>', IBLogon='<iBLogon>' WHERE CustomerId='<customerId>'",
-    INSERT: "INSERT INTO <tableName> (ProspectId,Cookie,SessionId,OtpEmailId,DomusCookieId,CustomerId,IBLogon) values (<prospectId>,<cookie>,<sessionId>,<otpEmailId>,<domusCookieId>,<customerId>,<iBLogon>)"
+    INSERT: "INSERT INTO <tableName> (ProspectId,Cookie,SessionId,OtpEmailId,DomusCookieId,CustomerId,IBLogon) values (<prospectId>,'<cookie>','<sessionId>','<otpEmailId>','<domusCookieId>',<customerId>,'<iBLogon>')"
 }
 
 // exporting modules, to be used in the other .js files
