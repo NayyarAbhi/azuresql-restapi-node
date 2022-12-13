f936bcfef02f5388e66a0b8f1e48182367b85098
hkswamy <kumarswamy.mscs@gmail.com>
2022-12-13 20:55:18 +0530

    find by api changes

--- src/main/variables/tables.js
+++ src/main/variables/tables.js
@@ -1,6 +1,6 @@
 const TABLES = {
-    PROSPECT: 'TestSchema.Prospect',
-    PROSPECT_IDENTIFIERS: 'TestSchema.Prospect_Identifiers'
+    PROSPECT: 'prospect.tbl_prospect',
+    PROSPECT_IDENTIFIERS: 'prospect.tbl_prospect_identifiers'
 }
 
 // exporting modules, to be used in the other .js files
