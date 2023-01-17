ed50b0c527a2da30905ae95866e4c0b6089e576d
hkswamy <kumarswamy.mscs@gmail.com>
2023-01-17 16:58:30 +0530

    index file updated with health urls

--- src/main/index.js
+++ src/main/index.js
@@ -25,6 +25,9 @@ app.use('/api/v1/prospect',
     prospect_information_route.prospectInformationRoutes
 
 );
+app.get('/readiness', (req,res) => res.status(HTTP.OK.code).json({message : 'Appilcation Ready to use'}));
+app.get('/liveness', (req,res) => res.status(HTTP.OK.code).json({message : 'Appilcation is live'}));
+app.get('/health', (req,res) => res.status(HTTP.OK.code).json({message : 'Appilcation is up and running'}));
 
 // unknown request
 app.get('*', (req, res) => res.status(HTTP.BAD_REQUEST.code).json({ message: 'not a valid request' }));
