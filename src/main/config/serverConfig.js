6ee93b8d08342d7b04dcbbf6f7305972fa4df459
NayyarAbhi <abhinayyar83@gmail.com>
2023-01-23 14:11:10 +0000

    server changes

--- src/main/config/serverConfig.js
+++ src/main/config/serverConfig.js
@@ -1,9 +1,9 @@
 // Create a configuration object for our Azure SQL connection parameters
 var dbConfig = {
-  server: "servermortgage.database.windows.net",
+  server: "servermortgages.database.windows.net",
   database: "Mortgages",
   user: "test",
-  password: "Password1@",
+  password: "Password@1",
   port: 1433,
   options: {
     encrypt: true
