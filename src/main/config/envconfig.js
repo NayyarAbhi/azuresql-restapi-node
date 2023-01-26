b5eb23750740aa3384e6b59714aa26965d42c39e
NayyarAbhi <abhinayyar83@gmail.com>
2023-01-26 11:58:35 +0000

    changes app config

--- src/main/config/envconfig.js
+++ src/main/config/envconfig.js
@@ -2,20 +2,17 @@ let environment = {
     LOCAL: {
         APP_PORT: '8085',
         APP_ENTRY_POINT: '/dataapi/prospect/',
-        APP_VERSION: 'v1',
-        DOMUSCOOKIE_STUB_URL: 'http://localhost:3000/domuscookie'
+        APP_VERSION: 'v1'
     },
     APPDEV: {
         APP_PORT: '8085',
         APP_ENTRY_POINT: '/dataapi/prospect/',
-        APP_VERSION: 'v1',
-        DOMUSCOOKIE_STUB_URL: 'http://http://prospect-management-ms-stub:3000/domuscookie'
+        APP_VERSION: 'v1'
     },
     INTEGRATION: {
         APP_PORT: '8085',
         APP_ENTRY_POINT: '/dataapi/prospect/',
-        APP_VERSION: 'v1',
-        DOMUSCOOKIE_STUB_URL: 'http://0.0.0.0:3000/domuscookie'
+        APP_VERSION: 'v1'
     }
 }
 
