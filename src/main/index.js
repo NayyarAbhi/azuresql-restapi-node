5be7f2f96eb08400c17b5b3b1c95a20d1f0fd07c
NayyarAbhi <abhinayyar83@gmail.com>
2023-01-26 15:15:19 +0000

    generic req

--- src/main/index.js
+++ src/main/index.js
@@ -12,5 +12,5 @@ app.get('/ready', (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilc
 app.get('/live', (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilcation is live' }));
 app.get('/health', (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilcation is up and running' }));
 
-// unknown request
-app.get('*', (req, res) => res.status(HTTP.BAD_REQUEST.code).json({ message: 'not a valid request' }));
+// generic request
+app.get(env.APP_ENTRY_POINT, (req, res) => res.status(HTTP.OK.code).json({ message: 'valid request' }));
