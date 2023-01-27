25eac8c166d000bccee596be97d746845ba80215
NayyarAbhi <abhinayyar83@gmail.com>
2023-01-27 12:06:27 +0000

    added entry point to health endpoint

--- src/main/index.js
+++ src/main/index.js
@@ -3,6 +3,7 @@ const app = require('./app.js');
 const env = require('./config/envconfig').env;
 
 const port = env.APP_PORT;
+const app_entry_point = env.APP_ENTRY_POINT
 
 app.listen(port, () => {
     console.log(`Server running on port:${port}`);
@@ -10,7 +11,7 @@ app.listen(port, () => {
 
 app.get('/ready', (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilcation Ready to use' }));
 app.get('/live', (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilcation is live' }));
-app.get('/health', (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilcation is up and running' }));
+app.get(`${app_entry_point}__health`, (req, res) => res.status(HTTP.OK.code).json({ message: 'Appilcation is up and running' }));
 
 // generic request
-app.get(env.APP_ENTRY_POINT, (req, res) => res.status(HTTP.OK.code).json({ message: 'valid request' }));
+app.get(app_entry_point, (req, res) => res.status(HTTP.OK.code).json({ message: 'valid request' }));
